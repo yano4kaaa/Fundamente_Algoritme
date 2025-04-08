ed884f0081c9b2bad01046320b85dad6a279b4d8
yano4kaaa <kova1evayana@yandex.ru>
2025-04-08 11:16:38 +0000

    created stands for count_vertices

---
+++ стенд_3_count_vertices.py
@@ -0,0 +1,12 @@
+from voln import count_vertices
+import timeit
+
+def test_multiple_edges():
+    """Тест с несколькими ребрами и общими вершинами."""
+    edges = [(1, 2), (2, 3), (3, 4), (2, 5)]
+    start = timeit.default_timer()
+    count_vertices(edges)
+    time = timeit.default_timer() - start
+    return ("%.10f" % time).rstrip('0')
+
+print(test_multiple_edges())
